94f944a7fb4c3c1bde5ecd3d298d5675be886aaa
Cean Crester Tolentino <ceancrestertolentino@Ceans-MacBook-Pro.local>
2022-02-23 16:00:03 -0800

    login and registration working

--- mycookingapp/urls.py
+++ mycookingapp/urls.py
@@ -15,6 +15,11 @@ urlpatterns = [
     # ------- RECIPES NEXT PAGE -------- #
     path('next', views.next),
     # ----- RENDER SHOW RECIPE ------ #
-    path('recipe/<int:recipeId>', views.recipe)
-    # ------ SEARCH RECIPE --------- #
+    path('recipe/<int:recipeId>', views.recipe),
+    # ------ LOGIN GET & POST --------- #
+    path('login', views.login),
+    # ------ REGISTER GET & POST --------- #
+    path('register', views.register),
+    # ------ LOGOUT USER ----------#
+    path('logout', views.logout)
 ]
--- mycookingapp/views.py
+++ mycookingapp/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, redirect
 import requests, math
+from django.contrib import messages
+from .models import User
+import bcrypt
 
 def index(request):
-    # url = 'https://api.spoonacular.com/recipes/complexSearch?instructionsRequired=true&apiKey=88aeec238f524b3aafb910b702333739'
-
-    # ####### movie information json ######
-    # r = requests.get(url)
-    # result = r.json()
-    # print(result['results'])
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     return render(request, 'index.html')
 
 def cuisine(request):
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     return render(request, "cuisine.html")
 
 def diet(request):
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     return render(request, "diet.html")
 
 # ------------------SHOW CUISINE RECIPES-----------------------
 def recipesCuisine(request,category, type, page):
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     offset = (page-1) * 10
     if request.method == 'POST':
         del request.session['searchKey']
@@ -59,6 +64,8 @@ def recipesCuisine(request,category, type, page):
 
 # ----------------- RECIPES PREVIOUS PAGE ----------------- #
 def prev(request):
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     page = int(request.POST['currentPage']) - 1
     category = request.POST['category']
     type = request.POST['type']
@@ -66,6 +73,8 @@ def prev(request):
 
 # ----------------- RECIPES NEXT PAGE ----------------- #
 def next(request):
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     page = int(request.POST['currentPage']) + 1
     category = request.POST['category']
     type = request.POST['type']
@@ -73,6 +82,8 @@ def next(request):
 
 # ------------------SHOW ONE RECIPE-----------------------
 def recipe(request, recipeId):
+    if 'userLoggedIn' not in request.session:
+        return redirect('/login')
     url = f'https://api.spoonacular.com/recipes/{recipeId}/information?apiKey=88aeec238f524b3aafb910b702333739'
     r = requests.get(url)
     recipe = r.json()
@@ -91,3 +102,48 @@ def recipe(request, recipeId):
     
     }
     return render(request, "recipe.html", context)
+
+# ------------------ SHOW & PROCESS LOGIN ---------------------- #
+def login(request):
+    if 'userLoggedIn' in request.session:
+        return redirect('/')
+    if request.method == 'POST':
+            user = User.objects.filter(email = request.POST['email'])
+            if len(user) > 0:
+                if bcrypt.checkpw(request.POST['password'].encode(), user[0].password.encode()):
+                    print(user[0].__dict__)
+                    request.session['userLoggedIn'] = user[0].__dict__['id']
+                    return redirect('/')
+                else:
+                    messages.add_message(request, messages.ERROR,"Invalid email/password!", extra_tags="loginError")
+                    return redirect('/login')
+            else:
+                messages.add_message(request, messages.ERROR,"Invalid email/password!", extra_tags="loginError")
+                return redirect('/login')
+    return render(request, 'login.html')
+
+# ------------------ LOGOUT --------------------- #
+def logout(request):
+    del request.session['userLoggedIn']
+    return redirect('/login')
+
+# ------------------ SHOW & PROCESS REGISTRATION ---------------------- #
+def register(request):
+    if 'userLoggedIn' in request.session:
+        return redirect('/')
+    if request.method == 'POST':
+        errors = User.objects.validator(request.POST)
+        if len(errors)>0:
+            for key, value in errors.items():
+                messages.add_message(request, messages.ERROR, value, extra_tags=key)
+            return redirect('/register')
+        else:
+            password = request.POST['password']
+            pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+            new_user = User.objects.create(first_name = request.POST['fname'],
+            last_name = request.POST['lname'], email = request.POST['email'],
+            password=pw_hash)
+            messages.add_message(request, messages.INFO,"User created!", extra_tags="userCreated")
+            return redirect('/login')
+    return render(request, 'register.html')
